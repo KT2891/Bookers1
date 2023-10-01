25c78e51f1fe7be53c0c74d2cd33acd36ae82537
Keishiro Takata <t.k.6.2.1995@gmail.com>
2023-10-01 08:05:07 +0000

    [Create] destroy method

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,33 @@
 class BooksController < ApplicationController
+
   def index
+    @books = Book.all
   end
+
+  def create
+    @book = Book.new(book_params)
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to "/books"
+  end
+
+  # ----------プライベートメソッド----------
+  private
+
+  # ストロングパラメータ設定
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
+  end
+
 end
