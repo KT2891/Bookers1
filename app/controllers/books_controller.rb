49f446f4265e90f386cef9f5dd266457e4361e84
Keishiro Takata <t.k.6.2.1995@gmail.com>
2023-10-02 01:47:47 +0000

    [Create] show page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,32 +2,46 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
+    # @book = Book.new(title: params[:title], body: params[:body])
     @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:alart] = @book.errors.full_messages
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
-
+    @book = Book.find(params[:id])
   end
 
   def edit
-
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to "/books"
+    redirect_to books_path
   end
+
 
   # ----------プライベートメソッド----------
   private
-
   # ストロングパラメータ設定
-  def list_params
-    params.require(:list).permit(:title, :body, :image)
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 
 end
