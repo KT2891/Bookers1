0dab853bd27acc39c8a637e76b72ffa8717b3861
Keishiro Takata <t.k.6.2.1995@gmail.com>
2023-10-02 05:27:04 +0000

    [Finish] Bookers1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,7 +29,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
       render :edit
     end
